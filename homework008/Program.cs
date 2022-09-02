72707e8462040f464a058695b1f0fb2dbfff04b3
Andrey Pakulov <anpa1984@yandex.ru>
2022-09-03 00:18:52 +0200

    homework 008

--- homework008/Program.cs
+++ homework008/Program.cs
@@ -1,18 +1,14 @@
-﻿// Напишите программу, которая на вход принимает число и выдаёт, 
-// является ли число чётным (делится ли оно на два без остатка).
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
 
-//  4 -> да
-// -3 -> нет
-//  7 -> нет
+Console.WriteLine("Введите число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = 1;
 
-Console.WriteLine("Введите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number % 2 == 0)
+for (int i = secondNumber; i <= firstNumber; i++)
 {
-   Console.WriteLine("Yes");
-}
-else
-{
-Console.WriteLine("No");
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
 }
